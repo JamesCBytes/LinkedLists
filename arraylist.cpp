f9d1af71ba419c278957e8d20ccc80299cf69cb5
Connor James <123933737+JamesCBytes@users.noreply.github.com>
2024-10-01 00:23:22 -0700

    initial commit

---
+++ arraylist.cpp
@@ -0,0 +1,63 @@
+template<class ItemType>
+ArrayList<ItemType>::ArrayList() : maxCount(DEFAULT_CAPACITY), itemCount(0){}
+
+template<class ItemType>
+ArrayList<ItemType>::ArrayList(const ArrayList<ItemType>& aList){
+	cout << "implement me!" << endl; //remove this and add the correct code
+}
+
+template<class ItemType>
+bool ArrayList<ItemType>::isEmpty() const{
+	cout << "implement me!" << endl; //remove this and add the correct code
+
+	return false;
+}
+
+template<class ItemType>
+int ArrayList<ItemType>::getLength() const{
+	return itemCount;
+}
+
+template<class ItemType>
+bool ArrayList<ItemType>::insert(int newPosition, const ItemType& newEntry){
+	bool ableToInsert = (newPosition >= 1) &&
+                            (newPosition <= itemCount + 1) &&
+                            (itemCount < maxCount);
+	if (ableToInsert){
+		for(int pos = itemCount + 1; pos > newPosition; pos--){
+			items[pos] = items[pos - 1];
+        	}
+        	items[newPosition] = newEntry;
+        	itemCount++;
+    	}
+    	return ableToInsert;
+}
+
+template<class ItemType>
+bool ArrayList<ItemType>::remove(int position){
+	cout << "implement me!" << endl; //remove this and add the correct code
+
+    	return false;
+}
+
+template<class ItemType>
+void ArrayList<ItemType>::clear(){
+	itemCount = 0;
+}
+
+template<class ItemType>
+ItemType ArrayList<ItemType>::getEntry(int position) const {
+	bool ableToGet = (position >= 1) && (position <= itemCount);
+
+	if (ableToGet){
+		return items[position];
+	} 
+	throw "Item not found";
+} 
+
+template<class ItemType>
+ItemType ArrayList<ItemType>::replace(int position, const ItemType& newEntry){
+    	cout << "implement me!" << endl; //remove this and add the correct code
+    	ItemType temp;
+    	return temp;
+}
